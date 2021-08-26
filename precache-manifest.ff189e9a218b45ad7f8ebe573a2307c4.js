662c61d588c36edbd2f260e635d7f6a6851f26a0
RomanAfanasev <45339525+RomanAfanasev@users.noreply.github.com>
2021-08-26 14:33:51 +0300

    Updates

---
+++ precache-manifest.ff189e9a218b45ad7f8ebe573a2307c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11de34676425bd08a5ac17bf959fd2da",
+    "url": "/FlyTech/index.html"
+  },
+  {
+    "revision": "250938bde5de369f1b3d",
+    "url": "/FlyTech/static/css/main.8b84d68a.chunk.css"
+  },
+  {
+    "revision": "9a854770eb9213cd6b65",
+    "url": "/FlyTech/static/js/2.537ea816.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/FlyTech/static/js/2.537ea816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250938bde5de369f1b3d",
+    "url": "/FlyTech/static/js/main.3789b695.chunk.js"
+  },
+  {
+    "revision": "baff2bbcd494c2d25f4f",
+    "url": "/FlyTech/static/js/runtime-main.65cf77c4.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/FlyTech/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "f646ab658cea3efc6b9cadb38686d6b4",
+    "url": "/FlyTech/static/media/flytech.f646ab65.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/FlyTech/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/FlyTech/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/FlyTech/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
